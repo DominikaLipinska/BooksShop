0c6f2e67184f138985ded2dacc928f2d825dc5e4
„Dominika <s23171@pjwstk.edu.pl>
2023-04-13 14:53:45 +0200

    Abstract class Employee added

---
+++ src/Employee.java
@@ -0,0 +1,14 @@
+import java.time.LocalDate;
+
+abstract class Employee extends Person {
+    private LocalDate empDate;
+    private float salary;
+
+    public Employee(String firstName, String latsName, String phoneNumber, String email, Adres adres, LocalDate empDate, float salary) {
+        super(firstName, latsName, phoneNumber, email, adres);
+        this.empDate = empDate;
+        this.salary = salary;
+    }
+
+    public abstract float getIncome();
+}
