f5e93c7db85749d4807cc9d45c512acf9471f817
„Dominika <s23171@pjwstk.edu.pl>
2023-04-13 18:56:13 +0200

    Salesman update

--- src/Salesman.java
+++ src/Salesman.java
@@ -2,7 +2,7 @@ import java.time.LocalDate;
 
 public class Salesman extends Employee{
     private int overtimeHour;
-    private static float overtimeRate;
+    private static float overtimeRate = 40;
 
     public Salesman(String firstName, String latsName, String phoneNumber, String email, Adres adres, LocalDate empDate, float salary) {
         super(firstName, latsName, phoneNumber, email, adres, empDate, salary);
