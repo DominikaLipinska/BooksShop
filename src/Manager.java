e3f580629ae1337ffc484fa44fe0fcef5efb6a20
„Dominika <s23171@pjwstk.edu.pl>
2023-04-19 17:16:05 +0200

    Manager overlapping part1

--- src/Manager.java
+++ src/Manager.java
@@ -3,13 +3,14 @@ import java.time.LocalDate;
 public class Manager extends Employee{
     public Float salSupplement;
 
-    public Manager(String firstName, String latsName, String phoneNumber, String email, Adres adres, LocalDate empDate, float salary) {
-        super(firstName, latsName, phoneNumber, email, adres, empDate, salary);
+    public Manager(LocalDate empDate, float salary,Float salSupplement) {
+        super(empDate, salary);
+        this.salSupplement = salSupplement;
     }
 
-    public Manager(String firstName, String latsName, String phoneNumber, String email, Adres adres, LocalDate empDate, float salary,float salSupplement) {
-        this(firstName, latsName, phoneNumber, email, adres, empDate, salary);
-        this.salSupplement = salSupplement;
+    //geterry
+    public Float getSalSupplement() {
+        return salSupplement;
     }
 
     @Override
