0c52f21efa8fd9a080ef4e0fa4c989c3779e2529
„Dominika <s23171@pjwstk.edu.pl>
2023-04-19 20:24:41 +0200

    Employee overlapping part2

--- src/Employee.java
+++ src/Employee.java
@@ -3,6 +3,7 @@ import java.time.LocalDate;
 abstract class Employee extends ObjectPlusPlus {
     private LocalDate empDate;
     private float salary;
+    public static String roleNameGeneralization = "generalization";
 
     public Employee(LocalDate empDate, float salary) {
         this.empDate = empDate;
@@ -12,12 +13,32 @@ abstract class Employee extends ObjectPlusPlus {
     public LocalDate getEmpDate() {
         return empDate;
     }
-
     public float getSalary() {
         return salary;
     }
+    public Person getPerson() throws Exception{
+        try {
+            ObjectPlusPlus[] obj = this.getLinks(roleNameGeneralization);
+            return  (Person)obj[0];
+        }catch (Exception e){
+            throw new Exception("The object ist not person");
+        }
+    }
 
+    @Override
+    public String toString() {
+        String info ="";
+        try {
+            info+= getPerson();
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+        info += "empDate: " + empDate + "\n";
+        info += "salary: " + salary + "\n";
+        info += "income: " + getIncome() + "\n";
 
+        return info;
+    }
 
     public abstract float getIncome();
 }
