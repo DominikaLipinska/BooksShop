a487913d614a8adfb53f279b3d394a472179190b
„Dominika <s23171@pjwstk.edu.pl>
2023-04-19 21:14:52 +0200

    abstract part1

--- src/AuthorsMeeting.java
+++ src/AuthorsMeeting.java
@@ -1,2 +1,43 @@
-public class AuthorsMeeting {
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AuthorsMeeting extends Event {
+    private boolean treats;
+
+    private static List<AuthorsMeeting> extent = new ArrayList<>();
+
+    public AuthorsMeeting(String name, LocalDateTime date, float price, boolean treats) {
+        super(name, date, price);
+        this.treats = treats;
+        addAuthorsMeeting(this);
+    }
+
+    private void addAuthorsMeeting(AuthorsMeeting meeting){
+        extent.add(meeting);
+    }
+
+    public static void showExtent() {
+        System.out.println("Extent of the class: " + ThemeParty.class.getName());
+
+        for (AuthorsMeeting meeting : extent) {
+            System.out.println(meeting);
+        }
+    }
+    public static void writeExtent(ObjectOutputStream stream) throws IOException {
+        stream.writeObject(extent);
+    }
+    public static void readExtent(ObjectInputStream stream) throws IOException, ClassNotFoundException {
+        extent = (ArrayList<AuthorsMeeting>) stream.readObject();
+    }
+
+
+    @Override
+    public String toString() {
+        String info = super.toString() + "\n"+ "Has treats: " + treats + "\n";
+        return info;
+    }
 }
