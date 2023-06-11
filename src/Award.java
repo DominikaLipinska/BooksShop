840aaf62d4719fd2992d3b1dd4d99c2c7ad3681f
„Dominika <s23171@pjwstk.edu.pl>
2023-06-11 17:49:35 +0200

    Award class - almost done

--- src/Award.java
+++ src/Award.java
@@ -7,36 +7,14 @@ import java.util.List;
 
 public class Award implements Serializable {
     private String name;
-    private List<AuthorAward> authAwards = new ArrayList<>(); //Asocja Author -> Awards (z atrybutem)
+    private List<AuthorAward> authAwards = new ArrayList<>();//Asocja Author -> Awards (z atrybutem) {Bag}
 
     private static List<Award> extent = new ArrayList<>();//Ekstensja
 
+    //Konstruktor
     public Award(String name) {
         this.name = name;
         addAward(this);
-    }
-
-    //Ekstensja
-    private void addAward(Award award){
-        extent.add(award);
-    }
-    public void removeAward(){
-        while (!authAwards.isEmpty()){
-            authAwards.get(0).removeAuthAward(authAwards.get(0));
-        }
-        extent.remove(this);
-    }
-    public static void showExtent(){
-        System.out.println("Extent of the class: " + Award.class.getName());
-
-        for (Award award : extent) {
-            System.out.println(award);
-        }
-    }
-
-    //Gettery
-    public String getName() {
-        return name;
     }
 
     //Asocja Author -> Awards (z atrybutem)
@@ -47,7 +25,28 @@ public class Award implements Serializable {
         return authAwards;
     }
 
-    //Ekstensja trwałość
+    //Gettery i Settery
+    public String getName() {
+        return name;
+    }
+
+    //Ekstensja
+    private void addAward(Award award){
+        extent.add(award);
+    }
+    public void removeAward(){
+        while (!authAwards.isEmpty()){
+            authAwards.get(0).removeAuthAward();
+        }
+        extent.remove(this);
+    }
+    public static void showExtent(){
+        System.out.println("Extent of the class: " + Award.class.getName());
+
+        for (Award award : extent) {
+            System.out.println(award);
+        }
+    }
     public static void writeExtent(ObjectOutputStream stream) throws IOException {
         stream.writeObject(extent);
     }
@@ -57,13 +56,13 @@ public class Award implements Serializable {
 
     @Override
     public String toString() {
-        String info = "";
+        String info = name + "\n";
         if(!authAwards.isEmpty()){
             info+= "Authors:\n";
             for (AuthorAward authAward:authAwards) {
                 try {
-                    info+= authAward.getAuthor().getPerson().getFirstName() +
-                            " "+authAward.getAuthor().getPerson().getLatsName()+"\n";
+                    info+= authAward.getAuthor().getPerson().getName() +"\nMoney award: "+
+                            authAward.getMoneyAward()+"\n";
                 } catch (Exception exception) {
                     exception.printStackTrace();
                 }
