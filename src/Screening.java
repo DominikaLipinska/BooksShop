8c62e98fcae6d0abaaf59e0d4d8f7437838a9c42
„Dominika <s23171@pjwstk.edu.pl>
2023-06-12 10:39:03 +0200

    Screening class - almost done

--- src/Screening.java
+++ src/Screening.java
@@ -1,18 +1,36 @@
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
+//toedit
 abstract public class Screening implements Serializable {
     private String title;
     private Book book;
 
+    private static List<Screening> screeningsExtent = new ArrayList<>();
+
+    //Konstruktor
     public Screening(Book book,String title) {
         this.title = title;
         this.book = book;
     }
 
+    //Ekstensja
+    private void addScreening(Screening screening){
+        if(!screeningsExtent.contains(screening)){
+            screeningsExtent.add(screening);
+        }
+    }
+    public void removeScreening(){
+        if(screeningsExtent.contains(this)){
+            screeningsExtent.remove(this);
+        }
+    }
+
+    //Gettery i Settery
     public String getTitle() {
         return title;
     }
-
     public String getBookAuthor(){
         try {
             return book.getAurhor().getPerson().getFirstName() + " " + book.getAurhor().getPerson().getLatsName();
