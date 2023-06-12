a7504653bec4ba5528188694f373cf216c0692de
„Dominika <s23171@pjwstk.edu.pl>
2023-06-12 16:37:16 +0200

    Manager class - almost done

--- src/Manager.java
+++ src/Manager.java
@@ -2,13 +2,14 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
-
+//toedit
 public class Manager extends Employee{
     public Float salSupplement;
-    private List<Shift> managedShift = new ArrayList<>();
-
+    private Shift managedShift; //Asocjacja Manager -> Shift(1-1)
+    private List<Event> events = new ArrayList<>(); //Asocjacja Manager -> Event (1-*)
 
     public Manager(LocalDate empDate, float salary,Float salSupplement,Person person) {
         super(empDate, salary,person);
@@ -18,6 +19,44 @@ public class Manager extends Employee{
     }
 
     private static List<Manager> extent = new ArrayList<>(); //Ekstensja
+
+    //Asocjacja Manager -> Shift(1-1)
+    public void  addManageShift(Shift shift){
+        if(managedShift==null){
+            managedShift=shift;
+        }
+    }
+    public void removeManageShift(){
+        if(managedShift!=null){
+            managedShift=null;
+        }
+    }
+
+    //Asocjacja Manager -> Event (1-*)
+    ////Workshop
+    public void organizeEvent(String name, LocalDateTime date, float price){
+        Event event = new Workshop(name,date,price);
+        if(!events.contains(event)){
+            events.add(event);
+        }
+        event.addManager(this);
+    }
+    ////ThemeParty
+    public void organizeEvent(String name, LocalDateTime date, float price, String topic){
+        Event event = new ThemeParty(name, date, price, topic);
+        if(!events.contains(event)){
+            events.add(event);
+        }
+        event.addManager(this);
+    }
+    ////AuthorsMeeting
+    public void organizeEvent(Author author,String name, LocalDateTime date, float price, boolean treats){
+        Event event = new AuthorsMeeting(author, name, date, price, treats);
+        if(!events.contains(event)){
+            events.add(event);
+        }
+        event.addManager(this);
+    }
 
     //Ekstensja
     private void addManager(Manager manager){
@@ -33,13 +72,6 @@ public class Manager extends Employee{
             System.out.println(manager);
         }
     }
-
-    //geterry
-    public Float getSalSupplement() {
-        return salSupplement;
-    }
-
-    //Ekstensja Trwałość
     public static void writeExtent(ObjectOutputStream stream) throws IOException {
         stream.writeObject(extent);
     }
@@ -47,20 +79,25 @@ public class Manager extends Employee{
         extent = (ArrayList<Manager>) stream.readObject();
     }
 
-
+    //Metody
     public void giveAuthorization(Salesman salesman){
         salesman.authorized = true;
     }
     public void removeAuthorization(Salesman salesman){
         salesman.authorized = false;
     }
-    public void  addManageShift(Shift shift){
-        managedShift.add(shift);
-    }
-
     public void giveDiscount(Book book,double discount) throws Exception {
         book.giveDiscount(discount);
     }
+    public WorkshopInstructor hireInstructor(Instructor instructor, Workshop workshop, float salary) throws Exception {
+        return new WorkshopInstructor(salary, workshop, instructor);
+    }
+
+    //Geterry
+    public Float getSalSupplement() {
+        return salSupplement;
+    }
+
 
     @Override
     public float getIncome() {
