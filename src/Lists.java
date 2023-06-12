50cb1a01a0e776d80609bc9b6aaf547727cbdd88
„Dominika <s23171@pjwstk.edu.pl>
2023-06-12 13:13:43 +0200

    Lists class - almost done

--- src/Lists.java
+++ src/Lists.java
@@ -6,53 +6,21 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
-
+//toedit
 public class Lists implements Serializable {
     private String name;
-
     private Map<String,Book> booksQualif = new TreeMap<>(); //Asocjacja Book -> isbn | Lists (kwalifikowana)
-
     private Client client; //Asocja Client -> Lists (1-*)
+    private List<Order> orders = new ArrayList<>(); //Asocjaca Lists -> Order (1-1)
 
     private static List<Lists> extent = new ArrayList<>(); //Ekstensja
 
+    //Konstruktor
     public Lists(String name,Client client) {
         this.name = name;
         this.client = client;
+        client.addList(this);
         addList(this);
-    }
-
-    public List<Book> getBooksList() {
-        List<Book> bookList = new ArrayList<>();
-        for (Book book :booksQualif.values()) {
-            bookList.add(book);
-        }
-        return bookList;
-    }
-
-    //Ekstencja i Asocja Client -> Lists (1-*)
-    private void addList(Lists list){
-        if(!extent.contains(list)){
-            extent.add(list);
-            client.addList(list);
-        }
-    }
-    public void removeList(){
-        if(extent.contains(this)){
-            extent.remove(this);
-            client.removeList(this);
-            for(Book b : booksQualif.values()) {
-                booksQualif.remove(b.getIsbn());
-                b.removeList(this);
-            }
-        }
-    }
-    public static void showExtent() {
-        System.out.println("Extent of the class: " + Lists.class.getName());
-
-        for (Lists list : extent) {
-            System.out.print(list);
-        }
     }
 
     //Asocjacja Book -> isbn | Lists (kwalifikowana)
@@ -73,7 +41,44 @@ public class Lists implements Serializable {
         booksQualif.remove(book.getIsbn());
     }
 
-    //Ekstensja Trwałość
+    //Asocjaca Lists -> Order (1-1)
+    public Order createOrder(Client client){
+        return new Order(client,this);
+    }
+    public void addOrder(Order order){
+        if(!orders.contains(order)){
+            orders.add(order);
+        }
+    }
+    public void removeOrder(Order order){
+        if(orders.contains(order)){
+            orders.remove(order);
+        }
+    }
+
+    //Ekstencja
+    private void addList(Lists list){
+        if(!extent.contains(list)){
+            extent.add(list);
+        }
+    }
+    public void removeList(){
+        if(extent.contains(this)){
+            extent.remove(this);
+            client.removeList(this);
+            for(Book b : booksQualif.values()) {
+                booksQualif.remove(b.getIsbn());
+                b.removeList(this);
+            }
+        }
+    }
+    public static void showExtent() {
+        System.out.println("Extent of the class: " + Lists.class.getName());
+
+        for (Lists list : extent) {
+            System.out.print(list);
+        }
+    }
     public static void writeExtent(ObjectOutputStream stream) throws IOException {
         stream.writeObject(extent);
     }
@@ -81,6 +86,14 @@ public class Lists implements Serializable {
         extent = (ArrayList<Lists>) stream.readObject();
     }
 
+    //Gettery i Settery
+    public List<Book> getBooksList() {
+        List<Book> bookList = new ArrayList<>();
+        for (Book book :booksQualif.values()) {
+            bookList.add(book);
+        }
+        return bookList;
+    }
 
     @Override
     public String toString() {
